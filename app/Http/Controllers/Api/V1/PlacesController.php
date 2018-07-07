a059aedaf9e389413c81132c00782c919901698f
Narek Tutikyan <ntutikyan@macadamian.com>
2018-07-07 23:11:12 +0400

    Places controller generated

--- app/Http/Controllers/Api/V1/PlacesController.php
+++ app/Http/Controllers/Api/V1/PlacesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
 
 class PlacesController extends Controller
 {
@@ -14,7 +15,7 @@ class PlacesController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(['status' => 'ok'], 200);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// TODO after implementing basic functionality add authentication.
+Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+    Route::resource('places', 'PlacesController', ['except' => ['create', 'edit']]);
+});
